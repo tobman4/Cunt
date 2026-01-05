2b22b1d503ce20f717915d7d855452a87a2d4b4a
Tobias Lisbakken <tl@braum.tobman.no>
2026-01-05 11:01:25 +0100

    add wikipedia client

--- src/Cunt/Program.cs
+++ src/Cunt/Program.cs
@@ -20,5 +20,9 @@ builder.Services.AddHostedService<RoleEnforcer>();
 builder.Services.AddDiscordGateway();
 
 await builder.Build().RunAsync();
+builder.Services.AddHttpClient<WikipediaClient>(e => {
+  e.BaseAddress = new("https://api.wikimedia.org");
+  e.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0"); // we lie <3
+});
 
 
---
+++ src/Cunt/WikipediaClient.cs
@@ -0,0 +1,36 @@
+using System.Net.Http.Json;
+using System.Net.Http;
+using System.Text.Json.Nodes;
+
+namespace Cunt;
+
+record Event(int Year, string Title);
+
+class WikipediaClient(HttpClient client) {
+
+  private readonly HttpClient _client = client;
+  private readonly Random _rng = new();
+
+  public async Task<Event> GetTodaysEvent() {
+    var today = DateTime.Now;
+    var day = today.Day.ToString().PadLeft(2,'0');
+    var month = today.Month.ToString().PadLeft(2,'0');
+
+
+    var data = await _client.GetFromJsonAsync<JsonObject>($"feed/v1/wikipedia/en/onthisday/all/{month}/{day}");
+    if(data is null)
+      throw new Exception("Got bad data :(");
+
+    var events = data["events"]!;
+    var index = _rng.Next(events.AsArray().Count());
+
+    var todaysEvent = events.AsArray()[index];
+
+    return new(
+      todaysEvent?["year"]?.GetValue<int>() ?? throw new Exception("Cant get year"),
+      todaysEvent?["text"]?.GetValue<string>() ?? throw new Exception("Cant get title")
+    );
+
+  }
+}
+
