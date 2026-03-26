6b337ebf78f795d6c79c703227ac596f82c2958a
Tobias Lisbakken <tl@braum.tobman.no>
2026-03-26 12:26:31 +0100

    Dont spam yet

--- src/Cunt/Program.cs
+++ src/Cunt/Program.cs
@@ -15,14 +15,14 @@ DotEnv.LoadEnvFile("./.env");
 var builder = Host.CreateApplicationBuilder(args);
 
 
-builder.Services.AddHostedService<RoleEnforcer>();
-
+// builder.Services.AddHostedService<RoleEnforcer>();
 builder.Services.AddDiscordGateway();
 
-await builder.Build().RunAsync();
+// builder.Services.AddHostedService<TTH>();
 builder.Services.AddHttpClient<WikipediaClient>(e => {
   e.BaseAddress = new("https://api.wikimedia.org");
   e.DefaultRequestHeaders.Add("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/143.0.0.0"); // we lie <3
 });
 
 
+await builder.Build().RunAsync();
